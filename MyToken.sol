5ed775d83fa9626b76065040087d43a3b0fe487e
himanshuchawla009 <himanshuchawla2014@gmail.com>
2018-04-03 21:33:24 +0530

    token vesting implemented by overriding transfer function in MyToken

--- MyToken.sol
+++ MyToken.sol
@@ -1,121 +1,6 @@
 pragma solidity ^0.4.18;
-
-contract SafeMath {
-     function safeMul(uint a, uint b) internal returns (uint) {
-          uint c = a * b;
-          assert(a == 0 || c / a == b);
-          return c;
-     }
-
-     function safeSub(uint a, uint b) internal returns (uint) {
-          assert(b <= a);
-          return a - b;
-     }
-
-     function safeAdd(uint a, uint b) internal returns (uint) {
-          uint c = a + b;
-          assert(c>=a && c>=b);
-          return c;
-     }
-}
-
-// Standard token interface (ERC 20)
-// https://github.com/ethereum/EIPs/issues/20
-contract Token is SafeMath {
-     // Functions:
-     /// @return total amount of tokens
-     function totalSupply() public constant returns (uint256 supply);
-
-     /// @param _owner The address from which the balance will be retrieved
-     /// @return The balance
-     function balanceOf(address _owner) public constant returns (uint256 balance);
-
-     /// @notice send `_value` token to `_to` from `msg.sender`
-     /// @param _to The address of the recipient
-     /// @param _value The amount of token to be transferred
-     function transfer(address _to, uint256 _value) public returns(bool);
-
-     /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
-     /// @param _from The address of the sender
-     /// @param _to The address of the recipient
-     /// @param _value The amount of token to be transferred
-     /// @return Whether the transfer was successful or not
-     function transferFrom(address _from, address _to, uint256 _value) public returns(bool);
-
-     /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
-     /// @param _spender The address of the account able to transfer the tokens
-     /// @param _value The amount of wei to be approved for transfer
-     /// @return Whether the approval was successful or not
-     function approve(address _spender, uint256 _value) public returns (bool success);
-
-     /// @param _owner The address of the account owning tokens
-     /// @param _spender The address of the account able to transfer the tokens
-     /// @return Amount of remaining tokens allowed to spent
-     function allowance(address _owner, address _spender) public constant returns (uint256 remaining);
-
-     // Events:
-     event Transfer(address indexed _from, address indexed _to, uint256 _value);
-     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-}
-
-contract StdToken is Token {
-     // Fields:
-     mapping(address => uint256) balances;
-     mapping (address => mapping (address => uint256)) allowed;
-     uint public supply = 0;
-
-     // Functions:
-     function transfer(address _to, uint256 _value) public returns(bool) {
-          require(balances[msg.sender] >= _value);
-          require(balances[_to] + _value > balances[_to]);
-
-          balances[msg.sender] = safeSub(balances[msg.sender],_value);
-          balances[_to] = safeAdd(balances[_to],_value);
-
-          Transfer(msg.sender, _to, _value);
-          return true;
-     }
-
-     function transferFrom(address _from, address _to, uint256 _value) public returns(bool){
-          require(balances[_from] >= _value);
-          require(allowed[_from][msg.sender] >= _value);
-          require(balances[_to] + _value > balances[_to]);
-
-          balances[_to] = safeAdd(balances[_to],_value);
-          balances[_from] = safeSub(balances[_from],_value);
-          allowed[_from][msg.sender] = safeSub(allowed[_from][msg.sender],_value);
-
-          Transfer(_from, _to, _value);
-          return true;
-     }
-
-     function totalSupply() public constant returns (uint256) {
-          return supply;
-     }
-
-     function balanceOf(address _owner) public constant returns (uint256) {
-          return balances[_owner];
-     }
-
-     function approve(address _spender, uint256 _value) public returns (bool) {
-          // To change the approve amount you first have to reduce the addresses`
-          //  allowance to zero by calling `approve(_spender, 0)` if it is not
-          //  already 0 to mitigate the race condition described here:
-          //  https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-          require((_value == 0) || (allowed[msg.sender][_spender] == 0));
-
-          allowed[msg.sender][_spender] = _value;
-          Approval(msg.sender, _spender, _value);
-
-          return true;
-     }
-
-     function allowance(address _owner, address _spender) public constant returns (uint256) {
-          return allowed[_owner][_spender];
-     }
-}
-
-contract MyToken is StdToken{
+import './StandardToken.sol';
+contract MyToken is StandardToken{
     
     string public constant name = "MyToken";
     string public constant symbol = "MFT";
@@ -123,28 +8,141 @@ contract MyToken is StdToken{
     uint public constant TOTAL_SUPPLY = 1300000000 * (1 ether / 1 wei);
     uint public constant DEVELOPERS_BONUS = 300000000 * (1 ether / 1 wei);
     uint public constant TOTAL_SOLD_TOKEN_SUPPLY_LIMIT = 1000000000* (1 ether / 1 wei);
-  uint public totalSoldTokens = 0;
+     uint public totalSoldTokens = 0;
     uint public constant PRICE = 30000;  // per 1 Ether
+    uint public supply = 0;
     
-   
+   address owner;
     function MyToken() {
         balances[this] = balances[this] + DEVELOPERS_BONUS;
         assert(TOTAL_SOLD_TOKEN_SUPPLY_LIMIT==1000000000 * (1 ether / 1 wei));
         
+        owner = msg.sender;
     }
     function buyTokens() public payable
     {
        
        uint newTokens = msg.value * PRICE;
        require(totalSoldTokens + newTokens <= TOTAL_SOLD_TOKEN_SUPPLY_LIMIT);
-
-        balances[msg.sender] += newTokens;
         supply+= newTokens;
         
         totalSoldTokens+= newTokens;
+       balances[this] -= newTokens;
+        balances[msg.sender] += newTokens;
+        
+       
         
        
     }
+     modifier onlyOwner(){
+        require(msg.sender == owner);
+        _;
+        
+    }
+    
+    //this 
+   
+   
+    struct BenfeciaryGrant {
+        uint value;
+        bool revokable;
+        uint startTime;
+        uint endTime;
+        uint cliffTime;
+        uint usedTokens;
+    }
+  mapping(address => BenfeciaryGrant) benfeciaryGrants;
+  //total vesting which is already done;
+  uint public totalVestedAmount;
+  function grantToBeneficiary(uint _value,address _beneficiaryAddress,bool _revokable,uint _startTime,uint _endTime,uint _cliff) onlyOwner
+  {
+      
+     
+     
+        benfeciaryGrants[_beneficiaryAddress].value = _value;
+    
+        benfeciaryGrants[_beneficiaryAddress].revokable = _revokable;
+        benfeciaryGrants[_beneficiaryAddress].startTime = _startTime;
+        benfeciaryGrants[_beneficiaryAddress].endTime = _endTime;
+        benfeciaryGrants[_beneficiaryAddress].cliffTime = _cliff;
+        totalVestedAmount=+_value;
+        balances[_beneficiaryAddress] = + _value;
+        balances[this] = - _value;
+        
+        
+        
+      
+  }
+  //calculating vested tokens at any timestamp.
+  function CalculateVestedTokens(address _beneficiaryAddress,uint _anyTimeStamp) public constant returns (uint) {
+      BenfeciaryGrant grant = benfeciaryGrants[_beneficiaryAddress];
+     
+      if(grant.value == 0 ){
+          return 0;
+      }
+      if( _anyTimeStamp < grant.cliffTime){
+          return 0;
+      }
+     if (_anyTimeStamp >= grant.endTime) {
+            return grant.value;
+        }
+        uint duration =  grant.endTime.sub(grant.startTime);
+        
+      
+     uint tokensMulTime = grant.value.mul((_anyTimeStamp.sub(grant.startTime)));
+    
+         
+     return tokensMulTime / duration;
+      
+      
+  }
+  /// overriden methods
+
+    function transfer(address _to, uint256 _value)  public  returns (bool success) {
+        if(benfeciaryGrants[msg.sender].value > 0){
+             BenfeciaryGrant grant = benfeciaryGrants[msg.sender];
+             require(grant.value != 0);
+              uint vestedTokens = CalculateVestedTokens(msg.sender,block.timestamp);
+          if(vestedTokens == 0){
+              return false;
+            }
+       
+          uint transferableTokens = vestedTokens.sub(grant.usedTokens);
+          require(transferableTokens >= _value);
+          if(transferableTokens == 0){
+              return false;
+          }
+        
+          grant.usedTokens = grant.usedTokens.add(_value);
+          totalVestedAmount = totalVestedAmount.sub(_value);
+      return super.transfer(_to, _value);
+            
+        }
+        else{
+            
+             return super.transfer(_to, _value);
+        }
+        
+     
+    }
+
+  
+  function getCurrentVestedTokens() public constant returns (uint){
+      return CalculateVestedTokens(msg.sender,block.timestamp);
+  }
+  function revokeVesting(address beneficiary) onlyOwner {
+      BenfeciaryGrant grant = benfeciaryGrants[beneficiary];
+     require(grant.revokable);
+     uint refund = grant.value.sub(grant.usedTokens);
+     totalVestedAmount = totalVestedAmount.sub(refund);
+     transfer(beneficiary,refund);
+     
+   
+      
+  }
+
+    
+ 
     function () payable {
         buyTokens();
     }
@@ -157,7 +155,3 @@ contract MyToken is StdToken{
         
     }
     }
-
-
-
-
